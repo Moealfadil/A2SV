d7a06517d927bcf290237424b1da2447fff19e9b
Moealfadil <142026026+Moealfadil@users.noreply.github.com>
2026-02-06 23:34:06 +0400

    question solved

---
+++ how-many-numbers-are-smaller-than-the-current-number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def smallerNumbersThanCurrent(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        sortedList= sorted(nums)
+        numLessThan=[]
+        for i in nums:
+            numLessThan.append(sortedList.index(i))
+        return numLessThan
