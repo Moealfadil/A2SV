24cddecf91f2a249d65cb215aa9c720cc63cb63f
Mohammed Elfadil <142026026+Moealfadil@users.noreply.github.com>
2026-02-11 23:45:27 +0400

    Time: 0 ms (100%), Space: 12.4 MB (89.19%) - LeetHub

---
+++ 0789-escape-the-ghosts/0789-escape-the-ghosts.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def escapeGhosts(self, ghosts, target):
+        """
+        :type ghosts: List[List[int]]
+        :type target: List[int]
+        :rtype: bool
+        """
+        distance = abs(target[0]) + abs(target[1])
+    
+        for ghost in ghosts:
+            ghost_distance = abs(ghost[0] - target[0]) + abs(ghost[1] - target[1])
+            if ghost_distance <= distance:
+                return False
+        return True
+
+        
