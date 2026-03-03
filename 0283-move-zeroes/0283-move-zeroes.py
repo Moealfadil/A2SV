c10580def1152dcb8812fab977d953d0846153bd
Mohammed Elfadil <142026026+Moealfadil@users.noreply.github.com>
2026-03-03 22:28:33 +0400

    Time: 5 ms (67.03%), Space: 13.7 MB (16.49%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -7,9 +7,7 @@ class Solution(object):
         i=0
         j=0
         for j in range(len(nums)):
-            if nums[j]==0:
-                continue
-            else:
+            if nums[j]!=0:
                 nums[i], nums[j]=nums[j], nums[i]
                 i+=1
         return nums
