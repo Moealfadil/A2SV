e65b5defc65bdf7d18f0462f96443a6f94b93bda
Mohammed Elfadil <142026026+Moealfadil@users.noreply.github.com>
2026-04-14 23:09:52 +0400

    Time: 44 ms (73.21%), Space: 19.1 MB (91.9%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,5 +1,11 @@
 class Solution:
+    def __init__(self):
+        self.store={}
     def fib(self, n: int) -> int:
         if n==0 or n==1:
             return n
-        return self.fib(n-1)+self.fib(n-2)
+        elif n in self.store:
+            return self.store[n]
+        ans=self.fib(n-1)+self.fib(n-2)
+        self.store[n]=ans
+        return ans
